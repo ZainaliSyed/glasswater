6ef787851d42403bd6a582e046b5b8002445d099
Muhammad Umair Moosani <32166460+MuhammadUmairMoosani@users.noreply.github.com>
2020-10-21 00:34:37 +0500

    cronJob

--- App.js
+++ App.js
@@ -1,34 +1,86 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, TextInput, Button} from 'react-native';
 import auth from '@react-native-firebase/auth';
 import Notification from './services/Notification';
+import database from '@react-native-firebase/database';
+const db = database().ref('fcmToken');
+
+const signInView = (setEmail, setPassword, onLogin, setSignIn) => {
+  return (
+    <View style={{marginHorizontal: 16}}>
+      <Text style={{textAlign: 'center', fontSize: 25, marginTop: 100}}>
+        Sign In
+      </Text>
+      <Text>Email</Text>
+      <TextInput
+        style={{backgroundColor: 'lightgrey'}}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <Text style={{marginTop: 20}}>Password</Text>
+      <TextInput
+        style={{backgroundColor: 'lightgrey', marginBottom: 20}}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <TouchableOpacity onPress={() => setSignIn(false)}>
+        <Text style={{textAlign: 'right'}}>Don't have an account, Sign Up</Text>
+      </TouchableOpacity>
+      <Button title="Login" onPress={onLogin} />
+    </View>
+  );
+};
+const signUpView = (setEmail, setPassword, onLogin, setSignIn) => {
+  return (
+    <View style={{marginHorizontal: 16}}>
+      <Text style={{textAlign: 'center', fontSize: 25, marginTop: 100}}>
+        Sign Up
+      </Text>
+      <Text>Email</Text>
+      <TextInput
+        style={{backgroundColor: 'lightgrey'}}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <Text style={{marginTop: 20}}>Password</Text>
+      <TextInput
+        style={{backgroundColor: 'lightgrey', marginBottom: 20}}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <TouchableOpacity onPress={() => setSignIn(true)}>
+        <Text style={{textAlign: 'right'}}>
+          Already have an account, Sign In
+        </Text>
+      </TouchableOpacity>
+      <Button title="Login" onPress={onLogin} />
+    </View>
+  );
+};
 
 function App() {
   // Set an initializing state whilst Firebase connects
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [signIn, setSignIn] = useState(true);
 
   // Handle user state changes
   function onAuthStateChanged(user) {
     setUser(user);
     if (initializing) setInitializing(false);
   }
-  function onSignup(user) {
+  function onSignup() {
+    if (!email && !password) return null;
     auth()
-      .createUserWithEmailAndPassword(
-        'jane.doe@example.com',
-        'SuperSecretPassword!',
-      )
+      .createUserWithEmailAndPassword(email, password)
       .then(() => {
         console.log('User account created & signed in!');
       })
       .catch((error) => {
         if (error.code === 'auth/email-already-in-use') {
-          console.log('That email address is already in use!');
+          alert('That email address is already in use!');
         }
 
         if (error.code === 'auth/invalid-email') {
-          console.log('That email address is invalid!');
+          alert('That email address is invalid!');
         }
 
         console.error(error);
@@ -36,35 +88,51 @@ function App() {
   }
 
   function onLogin() {
+    if (!email && !password) return null;
     auth()
-      .signInWithEmailAndPassword(
-        'jane.doe@example.com',
-        'SuperSecretPassword!',
-      )
-      .then(() => {
+      .signInWithEmailAndPassword(email, password)
+      .then((data) => {
         console.log('User account created & signed in!');
+        Notification.fcmToken((token) => {
+          db.push({email: data.user._user.email, token});
+        });
       })
       .catch((error) => {
         if (error.code === 'auth/email-already-in-use') {
-          console.log('That email address is already in use!');
+          alert('That email address is already in use!');
         }
 
         if (error.code === 'auth/invalid-email') {
-          console.log('That email address is invalid!');
+          alert('That email address is invalid!');
         }
 
         console.error(error);
       });
   }
   function logout() {
+    let user = auth().currentUser;
+    let userEmail = user._user.email;
+    db.once('value').then((snapshot) => {
+      let value = snapshot.val();
+      let objectValues = Object.values(value);
+      let objectKeys = Object.keys(value);
+      let filterData = objectValues.find(
+        (filData) => filData.email === userEmail,
+      );
+      if (filterData) {
+        database()
+          .ref(`fcmToken/${objectKeys[objectValues.indexOf(filterData)]}`)
+          .remove();
+      }
+    });
+
     auth()
       .signOut()
-      .then(() => console.log('User signed out!'));
+      .then((data) => console.log('User signed out!'));
   }
 
   useEffect(() => {
     Notification.hasPermission();
-    console.log(Notification.fcmToken());
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber; // unsubscribe on unmount
   }, []);
@@ -74,22 +142,17 @@ function App() {
   if (!user) {
     return (
       <View>
-        <TouchableOpacity onPress={onSignup}>
-          <Text>Signup</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={onLogin}>
-          <Text>Login</Text>
-        </TouchableOpacity>
+        {signIn
+          ? signInView(setEmail, setPassword, onLogin, setSignIn)
+          : signUpView(setEmail, setPassword, onSignup, setSignIn)}
       </View>
     );
   }
 
   return (
-    <View>
-      <Text>Welcome {user.email}</Text>
-      <TouchableOpacity onPress={logout}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
+    <View style={{marginHorizontal: 16, marginTop: 100}}>
+      <Text style={{marginBottom: 20, fontSize: 20}}>Welcome {user.email}</Text>
+      <Button onPress={logout} title="Logout" />
     </View>
   );
 }
--- functions/index.js
+++ functions/index.js
@@ -3,26 +3,32 @@ const functions = require('firebase-functions');
 admin.initializeApp(functions.config().firebase);
 
 exports.pushNotificationRN = functions.https.onRequest(async (req, res) => {
-  var payload = {
-    notification: {
-      title: 'ABC',
-      body: 'ABC',
-    },
-    data: {
-      type: 'notification',
-    },
-  };
   admin
-    .messaging()
-    .sendToDevice(
-      'ciK9fEFFTCikXi-iOZCHjN:APA91bGCYV3-7SL_ycyk_VpQEM_YWFJtAmOr76e2OrcGVY68dhUJCyU1Vr8JsA-bHKpWC0RFINloWRh4dwJXy-qfqYTDmJtAOMB0jxaMnEvpzp3P18mvlRniMjBh3rL59e1Cwwo_Dv_i',
-      payload,
-      {
-        // Required for background/quit data-only messages on iOS
-        contentAvailable: true,
-        // Required for background/quit data-only messages on Android
-        priority: 'high',
-      },
-    );
+    .database()
+    .ref('fcmToken')
+    .once('value')
+    .then((snapshot) => {
+      let value = snapshot.val();
+      let objectValues = Object.values(value);
+      for (let token of objectValues) {
+        console.log('TOKEN', token.token);
+        var payload = {
+          notification: {
+            title: 'Glass Water',
+            body: 'Drink a glass water',
+          },
+          data: {
+            type: 'notification',
+          },
+        };
+        admin.messaging().sendToDevice(token.token, payload, {
+          // Required for background/quit data-only messages on iOS
+          contentAvailable: true,
+          // Required for background/quit data-only messages on Android
+          priority: 'high',
+        });
+      }
+    });
+
   return res.send(200);
 });
